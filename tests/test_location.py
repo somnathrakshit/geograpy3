f523efe9c5d5d86e52db2b02de4acb9d11af37a3
Wolfgang Fahl <wf@bitplan.com>
2021-08-12 11:46:45 +0200

    adds wikidata Id duplicate test for cityManager

--- tests/test_location.py
+++ tests/test_location.py
@@ -173,7 +173,24 @@ class TestLocationHierarchy(unittest.TestCase):
         countriesByWikiDataId,_dup=cm.getLookup("wikidataid")
         self.assertTrue("Q30" in countriesByWikiDataId)
         
-
+    def handleWikidataException(self,ex):
+        '''
+        handle a Wikidata exception
+        Args:
+            ex(Exception): the exception to handle - e.g. timeout
+        '''
+        print(f"Wikidata test failed {ex}")
+        # only raise exception for real problems
+        raise ex
+    
+    def checkNoDuplicateWikidataIds(self,locationManager):
+        locationsByWikiDataId,duplicates=locationManager.getLookup("wikidataid")
+        if len(duplicates)>0:
+            for i,duplicate in enumerate(duplicates):
+                    print(f"{i}:{duplicate}")
+        self.assertEqual(len(duplicates),0)
+        return locationsByWikiDataId
+        
     def testCountryManagerFromWikidata(self):
         '''
         tests if the CountryManager id correctly loaded from Wikidata query result
@@ -183,17 +200,33 @@ class TestLocationHierarchy(unittest.TestCase):
             try:
                 countryManager=CountryManager.fromWikidata()
                 self.assertTrue(len(countryManager.countries)>=190)
-                countriesByWikiDataId,duplicates=countryManager.getLookup("wikidataid")
-                if len(duplicates)>0:
-                    for i,duplicate in enumerate(duplicates):
-                        print(f"{i}:{duplicate}")
-                self.assertEqual(len(duplicates),0)
+                countriesByWikiDataId=self.checkNoDuplicateWikidataIds(countryManager)
                 self.assertTrue("Q40" in countriesByWikiDataId)
                 
             except Exception as ex:
-                print(f"Wikidata test failed {ex}")
+                self.handleWikidataException(ex)
+                
                 
     def test_RegionManagerFromWikidata(self):
+        '''
+        tests the loading of the RegionManager from wikidata query results
+        '''
+        # wikidata query results are unreliable
+        if TestLocationHierarchy.inCI() or self.testWikidata:
+            try:
+                regionManager = RegionManager.fromWikidata()
+                #check amount of regions
+                self.assertTrue(len(regionManager.regions)>3500)
+                regionsByWikiDataId=self.checkNoDuplicateWikidataIds(regionManager)
+                # check if california is present
+                self.assertTrue("Q99" in regionsByWikiDataId)
+                ca=regionManager.getLocationByID("Q99")
+                self.assertIsNotNone(ca)
+                self.assertEqual(ca.name, "California")
+            except Exception as ex:
+                self.handleWikidataException(ex)
+
+    def test_CityManagerFromWikidata(self):
         '''
         tests the loading of the RegionManager from wikidata query results
         '''
@@ -201,37 +234,18 @@ class TestLocationHierarchy(unittest.TestCase):
         self.testWikidata=True
         if TestLocationHierarchy.inCI() or self.testWikidata:
             try:
-                regionManager = RegionManager.fromWikidata()
+                regions=["Q1198"]
+                cityManager=CityManager.fromWikidata(regionIDs=regions, fromBackup=False)
                 #check amount of regions
-                self.assertTrue(len(regionManager.regions)>3500)
-                regionsByWikiDataId,duplicates=regionManager.getLookup("wikidataid")
-                if len(duplicates)>0:
-                    for i,duplicate in enumerate(duplicates):
-                        print(f"{i}:{duplicate}")
-                self.assertEqual(len(duplicates),0)
-                # check if california is present
-                self.assertTrue("Q99" in regionsByWikiDataId)
-                ca=regionManager.getLocationByID("Q99")
-                self.assertIsNotNone(ca)
-                self.assertEqual(ca.name, "California")
+                self.assertTrue(len(cityManager.cities)>1300)
+                citiesByWikiDataId=self.checkNoDuplicateWikidataIds(cityManager)
+                self.assertTrue("Q1017" in citiesByWikiDataId)
+                # check if NRW is present (region of Germany)
+                aachen=cityManager.getLocationByID("Q1017")
+                self.assertIsNotNone(aachen)
+                self.assertEqual(aachen.name, "Aachen")
             except Exception as ex:
-                print(f"Wikidata test failed {ex}")
-                # only raise exception for real problems
-                raise ex
-
-    def test_CityManagerFromWikidata(self):
-        '''
-        tests the loading of the RegionManager from wikidata query results
-        '''
-        return  # wikidata query results are unreliable
-        regions=["Q1198"]
-        cityList=CityManager.fromWikidata(regionIDs=regions, fromBackup=False)
-        #check amount of regions
-        self.assertTrue(len(cityList.cities)>1300)
-        # check if NRW is present (region of Germany)
-        aachen=cityList.getLocationByID("Q1017")
-        self.assertIsNotNone(aachen)
-        self.assertEqual(aachen.name, "Aachen")
+                self.handleWikidataException(ex)
 
     def testCityManagerFromJSONBackup(self):
         '''
