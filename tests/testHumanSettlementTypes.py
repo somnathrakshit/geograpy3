7e936382a3f41da688e153c980aad09cdb05b20e
Wolfgang Fahl <wf@bitplan.com>
2021-08-16 19:15:17 +0200

    adds getting cities

--- tests/testHumanSettlementTypes.py
+++ tests/testHumanSettlementTypes.py
@@ -7,7 +7,7 @@ import unittest
 from tests.basetest import Geograpy3Test
 from geograpy.locator import RegionManager,LocationContext
 from geograpy.wikidata import Wikidata
-
+import json
 class TestHumanSettlementTypes(Geograpy3Test):
 
 
@@ -19,7 +19,7 @@ class TestHumanSettlementTypes(Geograpy3Test):
         pass
 
 
-    def testHsCounting(self):
+    def testCitiesByRegion(self):
         '''
         test counting human settlement types
         '''
@@ -27,30 +27,56 @@ class TestHumanSettlementTypes(Geograpy3Test):
         regionManager = RegionManager(config=config)
         regionManager.fromCache()
         wd=Wikidata()
-        limit=25
-        index=0
-        for region in regionManager.getList():
-            index+=1
+        limit=5000
+        regionList=regionManager.getList()   
+        total=len(regionList) 
+        for index,region in enumerate(regionList):
             regionId=region.regionId
-            queryString="""# human settlements in norther island
-SELECT distinct ?region ?regionLabel ?hsType ?hsTypeLabel ?hsCount WHERE { 
+            queryString="""SELECT distinct ?cityQ ?cityQLabel ?geoNameId ?gndId ?regionId ?countryId ?cityCoord ?cityPopulation WHERE { 
   VALUES ?hsType {
-      wd:Q1549591 wd:Q515 wd:Q1637706 wd:Q1093829 wd:Q486972 wd:Q532
+      wd:Q1549591 wd:Q3957 wd:Q5119 wd:Q15284 wd:Q62049 wd:Q515 wd:Q1637706 wd:Q1093829 wd:Q486972 wd:Q532
   }
-  {
-    
-    SELECT ?region ?hsType (count(?hs) as ?hsCount)
-    WHERE {   
-       VALUES ?region {
+  VALUES ?region {
          wd:%s
-       }  
-       ?hs wdt:P131* ?region.
-       ?hsType ^wdt:P279*/^wdt:P31 ?hs.
-    } GROUP by ?hsType ?region
-  }             
+  }  
+  ?cityQ wdt:P131* ?region.
+  ?hsType ^wdt:P279*/^wdt:P31 ?cityQ.
+   
+  # geoName Identifier
+  OPTIONAL {
+      ?cityQ wdt:P1566 ?geoNameId.
+  }
+
+  # GND-ID
+  OPTIONAL { 
+      ?cityQ wdt:P227 ?gndId. 
+  }
+  
+  OPTIONAL{
+     ?cityQ wdt:P625 ?cityCoord .
+  }
+  
+  # region this city belongs to
+  OPTIONAL {
+    ?cityQ wdt:P131 ?regionId .     
+  }
+  
+  OPTIONAL {
+     ?cityQ wdt:P1082 ?cityPopulation
+  }
+
+  # country this city belongs to
+  OPTIONAL {
+      ?cityQ wdt:P17 ?countryId .
+  }
   SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
 }""" % regionId
-            regionCounts=wd.query(f"getting hstype counts for {region.region}", queryString)
+            regionCities=wd.query(f"{index+1:4d}/{total:4d}:getting cities for {region.region}{region.regionIsoCode}", queryString)
+            cachePath=config.getCachePath()
+            jsonFileName=f"{cachePath}/regions/{region.regionIsoCode}.json"
+            jsonStr=json.dumps(regionCities)
+            with open(jsonFileName,"w") as jsonFile:
+                jsonFile.write(jsonStr)
             if index>=limit:
                 break
 
