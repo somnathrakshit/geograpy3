9ddaa4af66c0208e6861c3198fc5064b0978e5f9
Wolfgang Fahl <wf@bitplan.com>
2025-07-31 07:21:39 +0200

    prepares upgrade/release

--- geograpy/geograpy_nltk.py
+++ geograpy/geograpy_nltk.py
@@ -2,13 +2,19 @@
 # converted to python script 2024-03-29
 import nltk
 def main():
-    nltk.downloader.download('maxent_ne_chunker')
-    nltk.downloader.download('words')
-    nltk.downloader.download('treebank')
-    nltk.downloader.download('maxent_treebank_pos_tagger')
-    nltk.downloader.download('punkt')
-    # since 2020-09
-    nltk.downloader.download('averaged_perceptron_tagger')
+    """Download essential NLTK datasets."""
+    datasets = [
+        'punkt',
+        'punkt_tab', 
+        'averaged_perceptron_tagger',  # replaces maxent_treebank_pos_tagger
+        'maxent_ne_chunker',
+        'words',
+        'treebank'
+    ]
+
+    for dataset in datasets:
+        nltk.download(dataset)
+
 
 if __name__ == "__main__":
     main()
