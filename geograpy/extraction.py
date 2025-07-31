32d4047bd53cecaa2fd6f990385895757773d6a1
Wolfgang Fahl <wf@bitplan.com>
2025-07-31 08:29:21 +0200

    single point of truth for nltk downloads

--- geograpy/extraction.py
+++ geograpy/extraction.py
@@ -1,8 +1,9 @@
+#!/usr/bin/env python3
+# geograpy-nltk converted to python script 2024-03-29
+# using Extractor as the single point of truth since 2025-07-31
 import re
-
 import nltk
 from newspaper import Article
-
 from geograpy.labels import Labels
 
 
@@ -10,12 +11,10 @@ class Extractor(object):
     """
     Extract geo context for text or from url
     """
-
     def __init__(self, text=None, url=None, debug=False):
         """
         Constructor
         Args:
-
             text(string): the text to analyze
             url(string): the url to read the text to analyze from
             debug(boolean): if True show debug information
@@ -26,22 +25,27 @@ class Extractor(object):
         self.text = text
         self.url = url
         self.places = []
+        Extractor.provide_nltk_packages()
+
+    @staticmethod
+    def provide_nltk_packages():
+        """
+        Download required NLTK packages if not available
+        """
         nltk_packages = [
             "maxent_ne_chunker",
             "words",
             "treebank",
-            "maxent_treebank_pos_tagger",
             "punkt",
-            "averaged_perceptron_tagger",
+            "punkt_tab",
+            "averaged_perceptron_tagger_eng",  # Updated: language-specific
         ]
+
         for nltk_package in nltk_packages:
             try:
-                import nltk
-
                 nltk.data.find(nltk_package)
             except LookupError:
-                nltk.downloader.download(nltk_package, quiet=True)
-        import nltk
+                nltk.download(nltk_package, quiet=True)
 
     def set_text(self):
         """
@@ -56,7 +60,6 @@ class Extractor(object):
     def split(self, delimiter=r","):
         """
         simpler regular expression splitter with not entity check
-
         hat tip: https://stackoverflow.com/a/1059601/1497139
         """
         self.set_text()
@@ -65,7 +68,6 @@ class Extractor(object):
     def find_geoEntities(self):
         """
         Find geographic entities
-
         Returns:
             list:
                 List of places
@@ -84,10 +86,8 @@ class Extractor(object):
                 List of places
         """
         self.set_text()
-
         text = nltk.word_tokenize(self.text)
         nes = nltk.ne_chunk(nltk.pos_tag(text))
-
         for ne in nes:
             if type(ne) is nltk.tree.Tree:
                 nelabel = ne.label()
@@ -97,3 +97,10 @@ class Extractor(object):
                         print(leaves)
                     self.places.append(" ".join([i[0] for i in leaves]))
         return self.places
+
+def main():
+    """Download essential NLTK datasets"""
+    Extractor.provide_nltk_packages()
+
+if __name__ == "__main__":
+    main()
